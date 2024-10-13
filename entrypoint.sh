9516b3d20001877860493e4084e607e9931ca3d4
Diiego300years <marcin.buczak@onet.eu>
2024-10-13 17:57:48 +0200

    edit actions

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-# Uruchomienie migracji
-flask db init
+# Sprawdź, czy katalog 'migrations' istnieje, jeśli nie, uruchom 'flask db init'
+if [ ! -d "./migrations" ]; then
+  echo "Folder 'migrations' doesn't exist, I'm starting init..."
+  flask db init
+else
+  echo "Folder called 'migrations' exist."
+fi
+
+# Uruchom migracje i aktualizację bazy danych
 flask db migrate -m "docker test migrations"
 flask db upgrade
 
