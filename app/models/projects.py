c8053b68baf85da04192ec1bf021247ef94d5441
Marcin Buczak <93789555+Diiego300years@users.noreply.github.com>
2024-10-22 13:59:26 +0200

    fast upgrade (#8)

--- app/models/projects.py
+++ app/models/projects.py
@@ -2,6 +2,7 @@ from app import db
 import uuid
 from sqlalchemy.dialects.postgresql import UUID
 
+
 class MyProjects(db.Model):
     __tablename__ = 'my_project'
     id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
@@ -10,3 +11,10 @@ class MyProjects(db.Model):
     repo_link = db.Column(db.String(255), nullable=False)
     page_link = db.Column(db.String(255), nullable=False)
     graphics = db.Column(db.String(255), nullable=False)
+
+    def __repr__(self):
+        return (f'User(project_name={self.project_name},'
+                f'description={self.description},'
+                f'repo_link={self.repo_link},'
+                f'page_link={self.page_link},'
+                f'graphics={self.graphics})')
